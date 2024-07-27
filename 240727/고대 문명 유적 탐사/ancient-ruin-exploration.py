3a3d8162d1922fcbc92459ea0aeaea9f54bf0df4
Jaewon Cho <112532935+Jaejae1107@users.noreply.github.com>
2024-07-27 21:29:30 +0900

    Title: 고대 문명 유적 탐사, Time: 475ms, Memory: 28MB, Status: Passed - CodeTree

--- 240727/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 240727/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,69 +1,64 @@
 import sys
-import copy
+from copy import deepcopy
 from collections import deque
 
 
 
+# Constants for directions (right, left, down, up)
+di, dj = [0, 0, 1, -1], [1, -1, 0, 0]
+
+# Read input
 K, M = map(int, input().split())
-maze = [list(map(int, input().split())) for _ in range(5)]
-piece = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(5)]
+w = list(map(int, input().split()))
+walls = deque(w)
 
-def bfs(i, j):
-    global visited, chose
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
+# Function to perform BFS for clearing blocks
+def bfs(bi, bj, visit, num, b_arr):
     q = deque()
-    q.append([i, j])
-    visited[i][j] = True
-    chose.append([i, j])
-    cnt = 1
+    q.append((bi, bj))
+    r = [(bi, bj)]
     while q:
-        y, x = q.popleft()
+        si, sj = q.popleft()
+        for d in range(4):
+            ni, nj = si + di[d], sj + dj[d]
+            if 0 <= ni < 5 and 0 <= nj < 5 and not visit[ni][nj] and b_arr[ni][nj] == num:
+                q.append((ni, nj))
+                r.append((ni, nj))
+                visit[ni][nj] = True
 
-        for k in range(4):
-            ny = dy[k] + y
-            nx = dx[k] + x
-            if 0 <= ny < 5 and 0 <= nx < 5 and not visited[ny][nx] and pan[ny][nx] == pan[y][x] and pan[ny][nx] != 0:
-                visited[ny][nx] = True
-                chose.append([ny, nx])
-                q.append([ny, nx])
-                cnt += 1
+    if len(r) >= 3:
+        return len(r), r
+    else:
+        return 0, []
 
-    if cnt < 3:
-        for _ in range(cnt):
-            y, x = chose.pop()
-            visited[y][x] = False
+# Function to gain score by clearing blocks
+def gain_score(g_arr):
+    visited = [[False] * 5 for _ in range(5)]
+    temp_ans = 0
+    temp_r = []
+    for gi in range(5):
+        for gj in range(5):
+            if not visited[gi][gj]:
+                visited[gi][gj] = True
+                a, r = bfs(gi, gj, visited, g_arr[gi][gj], g_arr)
+                if a > 0:
+                    temp_r.extend(r)
+                    temp_ans += a
+    return temp_ans, temp_r
 
+# Function to fill blocks from the wall queue
+def fill_block(new_arr, route):
+    route.sort(key=lambda x: (x[1], -x[0]))
+    for fi, fj in route:
+        if walls:
+            val = walls.popleft()
+            new_arr[fi][fj] = val
+    return new_arr
 
-def bbfs(i, j):
-    global visited, cchose
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    qqq = deque()
-    qqq.append([i, j])
-    visited[i][j] = True
-    cchose.append([i, j])
-    cnt = 1
-    while qqq:
-        y, x = qqq.popleft()
-
-        for k in range(4):
-            ny = dy[k] + y
-            nx = dx[k] + x
-            if 0 <= ny < 5 and 0 <= nx < 5 and not visited[ny][nx] and fpan[ny][nx] == fpan[y][x]:
-                visited[ny][nx] = True
-                cchose.append([ny, nx])
-                qqq.append([ny, nx])
-                cnt += 1
-
-    if cnt < 3:
-        for _ in range(cnt):
-            y, x = cchose.pop()
-            visited[y][x] = False
-
-
-def rotate(y, x):
-    hey = copy.deepcopy(pan)
+# Function to perform 3x3 sub-grid rotation
+def rotate(y, x, pan):
+    hey = deepcopy(pan)
     hey[y + 1][x - 1] = pan[y + 1][x + 1]
     hey[y - 1][x - 1] = pan[y + 1][x - 1]
     hey[y - 1][x + 1] = pan[y - 1][x - 1]
@@ -72,88 +67,58 @@ def rotate(y, x):
     hey[y + 1][x] = pan[y][x + 1]
     hey[y][x - 1] = pan[y + 1][x]
     hey[y - 1][x] = pan[y][x - 1]
-
     return hey
 
+# Main explore function to find the best rotation
+def explore():
+    global arr
+    max_cnt = 0
+    best_center = (0, 0)
+    best_rotate = 3
+    best_route = []
+    best_arr = []
 
-realans = []
-for _ in range(K):
-    final = []
-    center = (0, 0)
-    angle = -1
-    ans = 0
     for i in range(1, 4):
         for j in range(1, 4):
-            pan = copy.deepcopy(maze)
-            for pp in range(3):
-                pan = rotate(i, j)
-                visited = [[False] * 5 for _ in range(5)]
-                chose = []
-                for q in range(5):
-                    for w in range(5):
-                        if not visited[q][w] and pan[q][w] != 0:
-                            bfs(q, w)
+            temp_arr = deepcopy(arr)
+            before_rotate = [row[j-1:j+2] for row in arr[i-1:i+2]]
+            for rot in range(4):
+                after_rotate = list(map(list, zip(*before_rotate[::-1])))
+                before_rotate = deepcopy(after_rotate)
+                r = 0
+                for ai in range(i-1, i+2):
+                    temp_arr[ai][j-1:j+2] = after_rotate[r]
+                    r += 1
 
-                if len(chose) > len(final):
-                    angle = pp
-                    center = (i, j)
-                    final = chose
-                    fpan = copy.deepcopy(pan)
-                elif len(chose) == len(final):
-                    if pp < angle:
-                        angle = pp
-                        center = (i, j)
-                        final = chose
-                        fpan = copy.deepcopy(pan)
-                    elif pp == angle:
-                        yy, xx = center
-                        if yy > i:
-                            angle = pp
-                            center = (i, j)
-                            final = chose
-                            fpan = copy.deepcopy(pan)
-                        elif yy == i:
-                            yy, xx = center
-                            if xx > j:
-                                angle = pp
-                                center = (i, j)
-                                final = chose
-                                fpan = copy.deepcopy(pan)
+                cnt, temp_route = gain_score(temp_arr)
+                if cnt > max_cnt or (cnt == max_cnt and (rot < best_rotate or (rot == best_rotate and (j < best_center[1] or (j == best_center[1] and i < best_center[0]))))):
+                    max_cnt = cnt
+                    best_center = (i, j)
+                    best_rotate = rot
+                    best_route = temp_route
+                    best_arr = deepcopy(temp_arr)
 
-  
-
-    while True:
-        ans += len(final)
-
-        for i in final:
-            y, x = i
-            fpan[y][x] = 0
-
-
-
-        for i in range(5):
-            for j in range(4, -1, -1):
-                if fpan[j][i] == 0:
-                    if piece:
-                        fpan[j][i] = piece.pop(0)
-
-        visited = [[False] * 5 for _ in range(5)]
-        cchose = []
-
-        for q in range(5):
-            for w in range(5):
-                if not visited[q][w] and fpan[q][w] != 0:
-                    bbfs(q, w)
-
-        final = cchose
-
-        if len(cchose) == 0:
-            break
-
-    maze = copy.deepcopy(fpan)
-    if ans != 0:
-        realans.append(ans)
+    if max_cnt == 0:
+        return -1
     else:
+        arr = deepcopy(best_arr)
+        while True:
+            arr = fill_block(arr, best_route)
+            cnt, temp_route = gain_score(arr)
+            if cnt > 0:
+                max_cnt += cnt
+                best_route = temp_route
+            else:
+                break
+        return max_cnt
+
+# Main loop for each K turns
+realans = []
+for _ in range(K):
+    score = explore()
+    if score == -1:
         break
+    else:
+        realans.append(score)
 
 print(*realans)
