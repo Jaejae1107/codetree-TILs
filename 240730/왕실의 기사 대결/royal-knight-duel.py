4c2e12cf7a35b91d4eef67cd858a8d6fc76ef790
Jaewon Cho <112532935+Jaejae1107@users.noreply.github.com>
2024-07-30 12:24:47 +0900

    Title: 왕실의 기사 대결, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/왕실의 기사 대결/royal-knight-duel.py
+++ 240730/왕실의 기사 대결/royal-knight-duel.py
@@ -1,148 +1,166 @@
+def night_move(oi, od):
+    r1, c1, h1, w1, k1 = night_info[oi]
+    if k1 <= 0: return False,[],[]
+    visited = [[False for _ in range(l)] for _ in range(l)]
+    queue = deque()
+    can_go = []
+    move_number = []
+    move_number.append(oi)
+    for i1 in range(r1, r1 + h1):
+        for j1 in range(c1, c1 + w1):
+            visited[i1][j1] = True
+            queue.append((i1, j1))
+            can_go.append((oi, i1, j1))
+    while queue:
+        x, y = queue.popleft()
+        nx, ny = x + dx[od], y + dy[od]
+        if in_range(nx, ny) and visited[nx][ny] == False and n_board[nx][ny] == oi:
+            queue.append((nx, ny))
+            can_go.append((nx, ny))
+            visited[nx][ny] = True
+        elif in_range(nx, ny) and visited[nx][ny] == False and board[nx][ny] == 2:
+            return False, [], []
+        elif in_range(nx, ny) and visited[nx][ny] == False and n_board[nx][ny] != oi:
+            if n_board[nx][ny] != 0:  # 빈공간이 아니라면
+                r1, c1, h1, w1, k1 = night_info[n_board[nx][ny]]
+                **move_number.append(n_board[nx][ny])**
+                for k1 in range(r1, r1 + h1):
+                    for k2 in range(c1, c1 + w1):
+                        queue.append((k1, k2))
+                        visited[k1][k2] = True
+                        can_go.append((n_board[nx][ny], k1, k2))
+        elif not in_range(nx, ny):
+            return False, [], []
+
+    return True, can_go, move_number
+
+
 import sys
-import copy
 from collections import deque
 
 
-L,N,Q = map(int, input().split())
-
-chess = [list(map(int, input().split())) for _ in range(L)] #0 빈칸, 1함정, 2벽
-health = [0] * (3 + N) #용사 체력
-knight = [[0] * L for _ in range(L)] # 기사 위치
-kloc = dict()
-firstk = 3
-for _ in range(N): #초기 기사 정보
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+l, n, q = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(l)]
+night_info = [0]
+n_board = [[0 for _ in range(l)] for _ in range(l)]
+damage = 0
+damaged_night= [ 0 for _ in range(n+1)]
+for nights in range(1, n + 1):
     r, c, h, w, k = map(int, input().split())
-    health[firstk] = k
-    kloc.update({firstk: []})
-    for i in range(r - 1, r + h - 1):
-        for j in range(c - 1, c + w - 1):
-            knight[i][j] = firstk
-            kloc[firstk].append([i,j])
-    firstk += 1
-
-start = copy.deepcopy(health) #기존 체력. 마지막에 빼기로 데미지 계산
+    r -= 1
+    c -= 1
+    night_info.append((r, c, h, w, k))
+    for i in range(r, r + h):
+        for j in range(c, c + w):
+            n_board[i][j] = nights
 
 
-def knightmove(kid,dirc):
-    global kloc, knight, changel
-    q = deque()
-    newkloc = []
-    move = []
-    tmp = copy.deepcopy(kloc)
-    a = kloc.pop(kid)
-    nocng = 0
-    kloc.update({kid: a})
-    for sena in a:
-        y, x = sena
-        knum = knight[y][x] #기사번호
-        q.append([y,x,knum])
-
-    while q:
-
-        y,x,knum = q.popleft()
-        ny = y + dy[dirc]
-        nx = x + dx[dirc]
-        if 0 > ny or ny >= L or 0 > nx or nx >= L:  # 만약 밖으로 빠져나가면 움직임 취소
-            nocng = 1
-            newkloc.clear()
-            break
-        if chess[ny][nx] == 2:  # 만약 벽이라면, 취소
-            newkloc.clear()
-            nocng = 1
-            break
-        if (chess[ny][nx] == 0 or chess[ny][nx] == 1) and (knight[ny][nx] == knum or knight[ny][nx] == 0): #아무것도 없다면
-            if knum not in move:
-                move.append(knum)
-            newkloc.append([ny,nx,knum]) #배치 완료
-        else:
-            nextknum = knight[ny][nx] #기존 기사 밀고
-            if knum not in move:
-                move.append(knum)
-            newkloc.append([ny, nx, knum])
+def in_range(x, y):
+    return 0 <= x < l and 0 <= y < l
 
 
-            kekeys = list(kloc.keys())
-            if nextknum in kekeys:
-                needck = kloc.pop(nextknum)
-            for sena in needck:
-                y, x = sena
-                knum = knight[y][x]  # 기사번호
-                q.append([y, x, knum])
+def night_move(oi, od):
+    r1, c1, h1, w1, k1 = night_info[oi]
+    if k1 <= 0: return False,[],[]
+    visited = [[False for _ in range(l)] for _ in range(l)]
+    queue = deque()
+    can_go = []
+    move_number = []
+    move_number.append(oi)
+    for i1 in range(r1, r1 + h1):
+        for j1 in range(c1, c1 + w1):
+            visited[i1][j1] = True
+            queue.append((i1, j1))
+            can_go.append((oi, i1, j1))
+    while queue:
+        x, y = queue.popleft()
+        nx, ny = x + dx[od], y + dy[od]
+        if in_range(nx, ny) and visited[nx][ny] == False and n_board[nx][ny] == oi:
+            queue.append((nx, ny))
+            can_go.append((nx, ny))
+            visited[nx][ny] = True
+        elif in_range(nx, ny) and visited[nx][ny] == False and board[nx][ny] == 2:
+            return False, [], []
+        elif in_range(nx, ny) and visited[nx][ny] == False and n_board[nx][ny] != oi:
+            if n_board[nx][ny] != 0:  # 빈공간이 아니라면
+                r1, c1, h1, w1, k1 = night_info[n_board[nx][ny]]
+                move_number.append(n_board[nx][ny])
+                for k1 in range(r1, r1 + h1):
+                    for k2 in range(c1, c1 + w1):
+                        queue.append((k1, k2))
+                        visited[k1][k2] = True
+                        can_go.append((n_board[nx][ny], k1, k2))
+        elif not in_range(nx, ny):
+            return False, [], []
 
-    if nocng == 1: # 바뀐게 없다면 원복
-        kloc = copy.deepcopy(tmp)
-
-    else:
-        a = list(kloc.keys())
-        for ja in move:
-            if ja in a:
-                kloc.pop(ja)
-            kloc.update({ja: []})
-
-        for i in newkloc:
-            y, x, knum = i
-            kloc[knum].append([y,x])
-            changel.append(knum)
-
-        knight = [[0] * L for _ in range(L)]  # 기사 위치 업데이트
-        keke = kloc.keys()
-        for jwa in keke:
-            b = kloc[jwa]
-            for jjwa in b:
-                y,x = jjwa
-                knight[y][x] = jwa
-
-    return nocng, changel
-def healthcare(movek,changel):
-    ban = []
-    for i in range(L):
-        for j in range(L):
-            if chess[i][j] == 1:
-                a = knight[i][j]
-                if a != movek and a in changel and health[a] > 0:
-                    health[a] -= 1
-                    if health[a] == 0:
-                        ban.append(a)
-                        kloc.pop(a)
+    return True, can_go, move_number
 
 
-    for i in range(L):
-        for j in range(L):
-            aa = knight[i][j]
-            if aa in ban:
-                knight[i][j] = 0
+def moving_night(arr, marr, oi, od):
+    global n_board
+    # arr안에있는녀석들을 od방향으로 밀어줘야한다.
+    t_nboard = [[0 for _ in range(l)] for _ in range(l)]
+
+    for i in range(l):
+        for j in range(l):
+            if not n_board[i][j] in marr:
+                t_nboard[i][j] = n_board[i][j]
+
+    for number, x, y in arr:
+        nx, ny = x + dx[od], y + dy[od]
+        t_nboard[nx][ny] = number
+
+    n_board = [[t_nboard[i][j] for j in range(l)] for i in range(l)]
+    return
 
 
 
+def damaged(m_arr, oi):
+    for i in range(l):
+        for j in range(l):
+            if n_board[i][j] == oi: continue
+            if n_board[i][j] == 0 : continue
+            if not n_board[i][j] in m_arr: continue
+            if board[i][j] == 1 :
+                r1, c1, h1, w1, k1 = night_info[n_board[i][j]]
+                k1 -= 1
+                damaged_night[n_board[i][j]] += 1
+                night_info[n_board[i][j]] = r1, c1, h1, w1, k1
+                if k1 <= 0 : # 맵 밀어줘야한다.
+                    for i1 in range(r1,r1+h1):
+                        for j1 in range(c1,c1+w1):
+                            n_board[i1][j1] = 0
+    return
+
+def update_night_info():
+    checked = []
+    for i in range(l):
+        for j in range(l):
+            if n_board[i][j] == 0 : continue
+            if not n_board[i][j] in checked:
+                checked.append(n_board[i][j])
+                r1, c1, h1, w1, k1 = night_info[n_board[i][j]]
+                night_info[n_board[i][j]] = i, j, h1, w1, k1
+    return
 
 
+def all_live_night_damaged():
+    count = 0
+    for night_num in range(1,n+1):
+        r1, c1, h1, w1, k1 = night_info[night_num]
+        if k1 > 0 :
+            count += damaged_night[night_num]
+    return count
 
 
-dx = [0,1,0,-1]
-dy = [-1,0,1,0]
-for _ in range(Q): #왕명
-    changel = []
-    i, d = map(int, input().split()) #i번 기사, d로 이동
-    i += 2 #기사 내가 쓰는걸로 업뎃
-    # print(kloc)
-    if health[i] != 0: #기사가 살아있다면
-        # for row in knight:
-        #     print(row)
-        # print("-----------")
-        # for row in chess:
-        #     print(row)
-        # print("------------")
-        # print(health)
-        # print(i,d)
-        valid, changel = knightmove(i,d) #기사들의 움직임 끝, 움직임 유무 리턴
-        if valid != 1:
-            healthcare(i, changel) #움직였다면, 체력 관리
-        # print("after healthcare")
-        # print(health)
+for order in range(q):
+    oi, od = map(int, input().split())
+    possible, can_arr, m_arr = night_move(oi, od)
+    if possible:
+        moving_night(can_arr, m_arr, oi, od)  # 움직일수있는애들 반영하기.
+        update_night_info()
+        damaged(m_arr, oi)
 
-ans = 0
-for i in range(1,N + 1):
-    if health[i + 2] > 0:
-        ans += start[i + 2] - health[i + 2]
-
-print(ans)
+print(all_live_night_damaged())
